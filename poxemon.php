ea2722d7ff161069a8681216c950814bf00e2208
Sean D. Sollé <git@solle.net>
2017-02-13 23:45:09 +0000

    Now gets code number from URL of embedding blogger page. Uses proper three-letter character names and placeholder Poxemon graphics. (We don't have the full set of graphics yet, so we repeat the ones we have).

--- poxemon.php
+++ poxemon.php
@@ -3,25 +3,32 @@
 error_reporting(-1);
 
 // Test whether we've been given a code
-if ( !isset($_REQUEST['code']) || empty($_REQUEST['code']))
+if ( !isset($_SERVER["HTTP_REFERER"]) || empty($_SERVER["HTTP_REFERER"]))
 {
-	// No code supplied - redirect to error page.
-         Header("Location: NoCodeError.html");
+	// Unable to determine referer - redirect to error page.
+         Header("Location: NoRefError.html");
          exit;
 }
 
-// Get our code fromn the URL param supplied, and check it's within range.
-$code = $_REQUEST['code'];
+// Get our code from the URL doing the embedding ...
+$code = basename($_SERVER["HTTP_REFERER"],".html");
 
-if ( $code < 0 || $code > 100)
+// ... and check it's within range.
+if ( $code < 1 || $code > 100)
 {
         // Code supplied is out of range - redirect to error page.
          Header("Location: BadCodeError.html");
          exit;
 }
 
-// One hundred names.
-$names = array("zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","forty","forty-one","forty-two","forty-three","forty-four","forty-five","forty-six","forty-seven","forty-eight","forty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine","sixty","sixty-one","sixty-two","sixty-three","sixty-four","sixty-five","sixty-six","sixty-seven","sixty-eight","sixty-nine","seventy","seventy-one","seventy-two","seventy-three","seventy-four","seventy-five","seventy-six","seventy-seven","seventy-eight","seventy-nine","eighty","eighty-one","eighty-two","eighty-three","eighty-four","eighty-five","eighty-six","eighty-seven","eighty-eight","eighty-nine","ninety","ninety-one","ninety-two","ninety-three","ninety-four","ninety-five","ninety-six","ninety-seven","ninety-eight","ninety-nine","one hundred");
+// One hundred names (plus dummy name zero).
+$names = array("","Abi","Agu","Ali","Ami","Ari","Ati","Bao","Bel","Bob","Cai","Cat","Chu","Dai","Daw","Dee","Des","Dua","Ebi","Efe","Eir","Eka","Eli","Eus","Fai","Fil","Gad","Gen","Gry","Hai","Hie","Ian","Ife","Ima","Ion","Ira","Iva","Jai","Jas","Jen","Jil","Jon","Jud","Kai","Kei","Keo","Kim","Kol","Lee","Liu","Liz","Lon","Luz","Mai","Meg","Mia","Moe","Nai","Nev","Nic","Nox","Oba","Oko","Oma","Ora","Osk","Pat","Peg","Qiu","Raj","Rao","Rik","Rui","Sam","Sei","Sia","Sol","Sue","Tad","Taj","Ted","Teo","Tim","Tor","Udo","Ulf","Una","Usi","Var","Viv","Vuk","Wid","Xan","Xue","Yaz","Yue","Yun","Zaw","Zhi","Zil","Zoe");
+
+// A list of filenames.
+$pngnames = array("noun_597393","vaccination-sign","wild-virus-sign","pox-noun_25857","biohazard_noun_78740","contact-tracing_noun_203956","edX-shadowpox-logo","isolation","light-shadow_noun_10635","noun_64228","noun_87978","noun_98901","noun_98903","noun_165966","noun_170457","noun_221949","pox-face_noun_38487","pox-noun_25857","shadow-double_noun_42858","Shadowpox-logo-1","Shadowpox-logo-2-shape","Shadowpox-logo-2","syringe_noun_7402","toropox-moon_black_noun_2700","toropox-moon_noun_2700","volunteer2");
+
+$pngcount = count($pngnames);
+
 
 // Pick 'n' random cards.
 
@@ -30,7 +37,12 @@ for ($i = 1; $i <= $code; $i++)
 
 	$pick = rand(1,100);
 
-	echo "<img src='PNGs/".$pick.".png' /><br/>";
+	// The png number is the pick modulo the number of images.
+	$png = ($pick-1) % $pngcount;
+
+
+
+	echo "<img width='100' height='100' src='PNGs/".$pngnames[$png].".png' /><br/>";
 	echo "<h2>".$names[$pick]."</h2>";
 	echo "<i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sapien lacus, dictum quis erat sed, vestibulum accumsan turpis. In vehicula dignissim est. Nunc et dui ullamcorper, ullamcorper ipsum a, consequat</i></br>";
 	echo "<hr/>";
