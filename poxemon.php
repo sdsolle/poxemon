772ec29089b0f5a5482b4514f9e85a00e7161798
Sean D. Sollé <git@solle.net>
2017-03-02 19:28:32 +0000

    Embedded URL from blogger or can be overridden by explicitly passing value as param. Renamed image folder 'PNGs' to the less-shouty 'png'. Image filename array now built from folder contents.

--- poxemon.php
+++ poxemon.php
@@ -2,16 +2,21 @@
 // Report all PHP errors
 error_reporting(-1);
 
-// Test whether we've been given a code
-if ( !isset($_SERVER["HTTP_REFERER"]) || empty($_SERVER["HTTP_REFERER"]))
+// By default, we've no code.
+$code = 0;
+
+// Use the refering URL to obtain the code.
+if (isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]))
 {
-	// Unable to determine referer - redirect to error page.
-         Header("Location: NoRefError.html");
-         exit;
+	// Get our code from the URL doing the embedding ...
+	$code = basename($_SERVER["HTTP_REFERER"],".html");
 }
 
-// Get our code from the URL doing the embedding ...
-$code = basename($_SERVER["HTTP_REFERER"],".html");
+// If we've been explicity passed a code as a paramater, overrride the one from the URL.
+if ( isset($_REQUEST['code']) && !empty($_REQUEST['code']))
+{
+	$code = $_REQUEST['code'];
+}
 
 // ... and check it's within range.
 if ( $code < 1 || $code > 100)
@@ -24,9 +29,10 @@ if ( $code < 1 || $code > 100)
 // One hundred names (plus dummy name zero).
 $names = array("","Abi","Agu","Ali","Ami","Ari","Ati","Bao","Bel","Bob","Cai","Cat","Chu","Dai","Daw","Dee","Des","Dua","Ebi","Efe","Eir","Eka","Eli","Eus","Fai","Fil","Gad","Gen","Gry","Hai","Hie","Ian","Ife","Ima","Ion","Ira","Iva","Jai","Jas","Jen","Jil","Jon","Jud","Kai","Kei","Keo","Kim","Kol","Lee","Liu","Liz","Lon","Luz","Mai","Meg","Mia","Moe","Nai","Nev","Nic","Nox","Oba","Oko","Oma","Ora","Osk","Pat","Peg","Qiu","Raj","Rao","Rik","Rui","Sam","Sei","Sia","Sol","Sue","Tad","Taj","Ted","Teo","Tim","Tor","Udo","Ulf","Una","Usi","Var","Viv","Vuk","Wid","Xan","Xue","Yaz","Yue","Yun","Zaw","Zhi","Zil","Zoe");
 
-// A list of filenames.
-$pngnames = array("noun_597393","vaccination-sign","wild-virus-sign","pox-noun_25857","biohazard_noun_78740","contact-tracing_noun_203956","edX-shadowpox-logo","isolation","light-shadow_noun_10635","noun_64228","noun_87978","noun_98901","noun_98903","noun_165966","noun_170457","noun_221949","pox-face_noun_38487","pox-noun_25857","shadow-double_noun_42858","Shadowpox-logo-1","Shadowpox-logo-2-shape","Shadowpox-logo-2","syringe_noun_7402","toropox-moon_black_noun_2700","toropox-moon_noun_2700","volunteer2");
+// Create a zero-index array containing files in png that aren't . or ..
+$pngnames = array_values(array_diff(scandir("../png"), array(".", "..")));
 
+//echo '<pre>'; print_r($pngnames); echo '</pre>';
 $pngcount = count($pngnames);
 
 // Create a shuffled deck of cards numbered 1-100
@@ -44,7 +50,7 @@ foreach ($deck as &$pick)
 	// The png number is the pick modulo the number of images.
 	$png = ($pick-1) % $pngcount;
 
-	echo "<img width='100' height='100' src='PNGs/".$pngnames[$png].".png' /><br/>";
+	echo "<img src='/png/".$pngnames[$png]."' /><br/>";
 	echo "<h2>".$names[$pick]."</h2>";
 	echo "<i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sapien lacus, dictum quis erat sed, vestibulum accumsan turpis. In vehicula dignissim est. Nunc et dui ullamcorper, ullamcorper ipsum a, consequat</i></br>";
 	echo "<hr/>";
