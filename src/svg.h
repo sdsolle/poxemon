6ba282c459df0c81170fb295a824d76a04498c72
Sean D. Sollé <git@solle.net>
2017-08-22 12:41:22 +0100

    Initial commit.

---
+++ src/svg.h
@@ -0,0 +1,14 @@
+//
+//  svg.h
+//  poxemon
+//
+//  Created by Sean D. Sollé on 22/08/2017.
+//  Copyright © 2017 Sean D. Sollé. All rights reserved.
+//
+
+#ifndef svg_h
+#define svg_h
+
+int RenderSVG();
+
+#endif /* svg_h */
